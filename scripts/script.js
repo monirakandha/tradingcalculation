76ec8abc5e9813137f2d6c203d850f4fbe39b0b1
monirhosenakanda <monirhosenakanda@gmail.com>
2022-02-19 16:28:35 +0600

    deposite input area done with out funciton also error handle clear

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
 const depositeBtn = document.getElementById('deposit-btn');
+const buyBtc = document.getElementById('buy-btc');
+const buyEth = document.getElementById('buy-eth');
+const balanceField = document.getElementById('balance-field');
+const btcField = document.getElementById('btc-field');
+const ethField = document.getElementById('eth-field');
+
+
+depositeBtn.addEventListener('click',function(event){
+    event.preventDefault();
+    let depositeBox = document.getElementById('deposit-box');
+    let avalableBalance = parseFloat(balanceField.innerText);
+    let inputAmount = parseFloat (depositeBox.value)
+    if(isNaN(inputAmount) || inputAmount < 0 ){
+        depositeBox.value = '';
+        alert('PLease Input Valid Amount')
+    }
+    balanceField.innerText = avalableBalance + inputAmount ;
+    depositeBox.value = '';
+
+})
